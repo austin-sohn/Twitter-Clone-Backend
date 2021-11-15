9d37f8bfa6f06fd7abe68eaa9f00e71e11a7432d
Austin S <56807941+austin-sohn@users.noreply.github.com>
2021-11-14 18:34:08 -0800

    Add files via upload
    Likes

--- Project2/hug/api/likes.py
+++ Project2/hug/api/likes.py
@@ -1,64 +1,86 @@
-# Users should be able to:
-# - like posts
-# - see how many likes a post has received
-# - retrieve a list of the posts that another user liked
-# - see a list of popular posts that were liked by many users
+# Users should be able to like posts
+# see how many likes a post has received
+# retrieve a list of the posts that another user liked
+# see a list of popular posts that were liked by many users.
 
 import configparser
-import logging.config
-
+#import hiredis
 import hug
-import json
+import logging.config
 import redis
 import sqlite_utils
-import timelines
 
 config = configparser.ConfigParser()
 config.read("./etc/timelines.ini")
 logging.config.fileConfig(config["logging"]["config"], disable_existing_loggers=False)
 
-redis = redis.Redis(host='localhost', port=6379, db=0)
-redis.flushall() # deletes db when ran
+#reader = hiredis.Reader()
+port = 6379
+redisClient = redis.Redis(host='localhost', port=port, db=0)
+redisClient.flushall() # deletes db when ran
 post = "post"
 
 @hug.directive()
 def sqlite(section="sqlite", key="dbfile", **kwargs):
     dbfile = config[section][key]
     return sqlite_utils.Database(dbfile)
-    
-# Adds all id to redis db and sets each like count to 0.
+
+
+# adds all id to redis db and sets each like count to 0
 @hug.local()
-def fill():
-    db = sqlite()
+def fill(db:sqlite):
     for row in db["posts"].rows:
-        id =  row["id"]
-        redis.zadd(post,{id:0})
+        username = row["username"]
+        post_id = row["post_id"]
+        url = "/likes/"+username+"/"+str(post_id)
+        redisClient.zadd(post,{url:0})
 fill()
 
-# Like a post.
-@hug.post("/timelines/{username}/{post_id}/like")
-def like(response, db:sqlite, username: hug.types.text, post_id: hug.types.number): 
+# like a post
+@hug.post("/likes/{username}/{post_id}")
+def like(response, username: hug.types.text, post_id: hug.types.text): 
+    url = "/likes/"+username+"/"+post_id
     try:
-        id = timelines.getUserID(db, username)
-        print(id)
-        redis.zincrby(post, 1, id)
+        redisClient.zincrby(post, 1, url)
     except:
         response.status = hug.falcon.HTTP_404
-    return {"liked": {"id": id, "username": username, "post_id": post_id}}
+    return {"Liked": {"url": url}}
 
-# See how many likes a post has received.
-@hug.get("/timelines/lcount/{id}")
-def like_counts(id: hug.types.number):
-    output = redis.zscore(post, id)
-    print(output)
-    return {"NumLikes":output}
+# see amount of likes on a post
+@hug.get("/likes/count/{username}/{post_id}")
+def like_counts(username: hug.types.text, post_id: hug.types.text):
+    url = "/likes/"+username+"/"+post_id
+    output = redisClient.zscore(post,url)
+    return {"Number of Likes":output}
 
-# Retrieve a list of the posts that another user has liked.
-#@hug.get()
-#def get_specific_likes()
+# see posts a user liked
+# TODO
+@hug.get("/likes/{username}/") 
+def user_liked(username: hug.types.text):
 
-# See a list of popular posts that were liked by many users.
-@hug.get("/timelines/popular")
+
+    output = 'idk'
+
+    return {username: output}
+
+# see list of popular a post
+@hug.get("/likes/popular")
 def popular_post():
-    output = redis.zrange(post,0,-1, withscores=True)
+    output = redisClient.zrevrange(post,0,-1, withscores=True)
     return {"popular":output}
+"""
+@hug.get("/timelines/health")
+def checkHealth(response, db: sqlite):
+    try:
+        posts = {"posts": db["posts"].rows_where()}
+        return posts
+    except Exception as e:
+        response.status = hug.falcon.HTTP_409
+        return {"error": str(e)}
+  
+@hug.startup()
+def selfRegister(api):
+    registerURL = "http://localhost:8000/registry/posts"
+    url = "http://" + socket.gethostbyname(socket.gethostname()) + ":" + os.environ["PORT"] + "/timelines" 
+    r = requests.post(registerURL, data={"text": url})
+"""
