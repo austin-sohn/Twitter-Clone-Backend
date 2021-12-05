82d558ea9d528ef17e693601f2635626c7ca6035
Vhuynh25 <43254910+Vhuynh25@users.noreply.github.com>
2021-12-04 16:22:36 -0800

    Delete Project2/hug/api directory

--- Project2/hug/api/bin/post.sh
+++
@@ -1,5 +0,0 @@
-# examples for creating post
-http localhost:8000/timeline/bob123/post text="today sucks!"
-http localhost:8000/timeline/terraria2/post text="i like games"
-http localhost:8000/timeline/brandon2306/post text="i agree!" url ="/timeline/bob123/1"
-
